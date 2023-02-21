4500b0f506ef7a890f4bfd0fa55c7f45ac6cff6f
ALexd-78 <alexd-78@yandex.ru>
2023-02-21 23:58:59 +0300

    Прописал фикстуру в conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,23 @@
 #импорты
-# import pytest
-#
-# @pytest.fixture()
-# def test_
+import pytest
+from utils import Item
+
+@pytest.fixture()
+def item():
+    return Item("Смартфон", 10000, 5)
+
+
+def test_item_initialized(item):
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 5
+
+
+def test_calculate_total_price():
+    assert item.calculate_total_price() == 50000
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.5
+    item.apply_discount()
+    assert item.price == 5000
