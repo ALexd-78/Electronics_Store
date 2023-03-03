71941528157c30818096171b266dbe54289ee606
ALexd-78 <alexd-78@yandex.ru>
2023-03-04 00:45:13 +0300

    Добавлены магические методы __repr__ и __str__  в utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,32 +1,39 @@
-import csv
-
 import pytest
-
 from utils import Item
 
-def test_calculate_total_price():
+@pytest.fixture()
+def item():
+    return Item("Смартфон", 10000, 5)
+
+
+def test_item_initialized(item):
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 5
+def test_calculate_total_price(item):
     '''Проверяет общую сумму всех товаров одной категории в магазине'''
-    item1 = Item("Смартфон", 10000, 20)
-    assert item1.calculate_total_price() == 160000
+    assert item.calculate_total_price() == 50000
 
 
-def test_apply_discount():
+def test_apply_discount(item):
     '''Проверяет сумму товара одной категории в магазине'''
-    item1 = Item("Смартфон", 10000, 20)
-    assert item1.apply_discount() == 8000
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000
 
 
-def test_name():
-    item1 = Item("Смартфон", 10000, 20)
-    assert item1.name == "Смартфон"
+def test_name(item):
+    assert item.name == "Смартфон"
 
 
 
-def test_name_setter():
-    item2 = Item("СуперСмартфон", 10000, 20)
-    assert item2.name == "Exception: Длина наименования товара превышает 10 символов."
-
+def test_name_setter(item):
+    with pytest.raises(Exception):
+        item.name = 'СуперСмартфон'
 
 def test_is_integer():
-    assert Item.is_integer(6.0) == True
-    assert Item.is_integer(6.3) == False
+    assert Item.is_integer(5) is True
+    assert Item.is_integer(5.0) is True
+    assert Item.is_integer(5.5) is False
+
+
